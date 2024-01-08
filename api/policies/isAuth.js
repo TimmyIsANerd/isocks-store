384ca10fa9ba7e33500d4b1ca2b90c02b3c07a9b
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2024-01-08 23:04:35 +0100

    chore: set up policy for authentication

---
+++ api/policies/isAuth.js
@@ -0,0 +1,50 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async function (req, res, proceed) {
+  if (req.header("authorization")) {
+    const token = req.header("authorization").split("Bearer ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        error: "Unathorized",
+        message: "No Token Provided",
+      });
+    }
+
+    return jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+      if (err) {
+        return res.status(401).json({
+          error: "Unauthorized",
+          message: "Invalid/Expired Token",
+        });
+      }
+
+
+      if (!payload.user) {
+        return res.status(401).json({
+          error: "Unauthorized",
+          message: "No user ID present in token",
+        });
+      }
+
+      const userRecord = await User.findOne({
+        id:payload.user
+      })
+
+      if(!userRecord){
+        return res.status(404).json({
+          error: "Resource Not Found",
+          message: "User does not exist in Database",
+        });
+      }
+
+      req.user = userRecord.id
+      return proceed();
+    });
+  }
+
+  return res.status(401).json({
+    status: "Unauthorized",
+    message: "Unauthorized Access from client side",
+  });
+};
